6c7d561432578858adea6bda0b126fc1c6e6cc17
Bobby Erickson <ericksonr07@gmail.com>
2015-01-22 20:36:39 -0500

    initial hangman commit

---
+++ hangman.js
@@ -0,0 +1,31 @@
+var hangmanWords = [
+"the","of","and","a","to","in","is","you","that","it","he",
+"was","for","on","are","as","with","his","they","I","at","be",
+"this","have","from","or","one","had","by","word","but","not",
+"what","all","were","we","when","your","can","said","there",
+"use","an","each","which","she","do","how","their","if","will",
+"up","other","about","out","many","then","them","these","so",
+"some","her","would","make","like","him","into","time","has",
+"look","two","more","write","go","see","number","no","way",
+"could","people","my","than","first","water","been","call",
+"who","oil","its","now","find","long","down","day","did","get",
+"come","made","may","part"
+];
+
+var largeWords = hangmanWords.filter(function(word){
+  return word.length >= 3;
+})
+
+var randomWords = largeWords[Math.floor(Math.random() * largeWords.length)];
+
+console.log(randomWords);
+
+var displayWord = randomWords.split('');
+
+var dashedWord = displayWord.map(function(ch){
+  return '_';
+});
+
+var finalDisplay = dashedWord.join(' ')
+
+//
